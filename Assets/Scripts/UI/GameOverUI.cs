1ba0e4acbd307f73f7d37c71f82a53a092867d23
Neur0mancer <plsportz@gmail.com>
2023-05-05 22:09:28 +0300

    Game over \ Restart

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+    [SerializeField] private Button replayButton;
 
+    private void Awake() {
+        replayButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+    }
     private void Start() {
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
         Hide();
